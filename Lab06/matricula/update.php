8c01708df98ed1e7aafbffddda6a669d9b84fae7
GabrielReyes16 <114525321+GabrielReyes16@users.noreply.github.com>
2023-05-04 00:00:26 -0500

    Add files via upload

---
+++ Lab06/matricula/update.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Editar Matricula</title>
+    <style>
+        body {
+            background-color: ghostwhite ;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .container {
+            background-color: white;
+            border-radius: 10px;
+            padding: 20px;
+            text-align: center;
+            border: 2px solid #000000;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+            max-width: 50%;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+    <h1>Editar Matricula</h1>
+    <h2>Nada aquí... lo siento :c </h2>
+        <a class="btn btn-outline-danger" type="button" href="read_matricula.php">Cancelar</a>
+    </div>
+</body>
+</html>
